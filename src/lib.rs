af191a322a77eb61168b18736a335e804a78614b
Josh Cheek <josh.cheek@gmail.com>
2015-05-24 17:33:29 -0600

    Refactor to match statement

--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,21 @@ pub fn escape_string(str:&str) -> String {
 }
 
 pub fn escape_char(c:char) -> String {
-       if c == '\u{0}'  { "\\x00".to_string() }
-  else if c == '\u{1}'  { "\\x01".to_string() }
-  else if c == '\u{2}'  { "\\x02".to_string() }
-  else if c == '\u{3}'  { "\\x03".to_string() }
-  else if c == '\u{4}'  { "\\x04".to_string() }
-  else if c == '\u{5}'  { "\\x05".to_string() }
-  else if c == '\u{6}'  { "\\x06".to_string() }
-  else if c == '\u{7}'  { "\\a".to_string() }
-  else if c == '\u{8}'  { "\\b".to_string() }
-  else if c == '\u{9}'  { "\\t".to_string() }
-  else if c == '\u{a}' { "\\n".to_string() }
-  else if c == '\u{b}' { "\\v".to_string() }
-  else if c == '\u{c}' { "\\f".to_string() }
-  else if c == '\u{d}' { "\\r".to_string() }
-  else                  { format!("NOTHING FOR {:?}", c) }
+  match c {
+    '\u{0}' => { "\\x00".to_string() },
+    '\u{1}' => { "\\x01".to_string() },
+    '\u{2}' => { "\\x02".to_string() },
+    '\u{3}' => { "\\x03".to_string() },
+    '\u{4}' => { "\\x04".to_string() },
+    '\u{5}' => { "\\x05".to_string() },
+    '\u{6}' => { "\\x06".to_string() },
+    '\u{7}' => { "\\a".to_string()   },
+    '\u{8}' => { "\\b".to_string()   },
+    '\u{9}' => { "\\t".to_string()   },
+    '\u{a}' => { "\\n".to_string()   },
+    '\u{b}' => { "\\v".to_string()   },
+    '\u{c}' => { "\\f".to_string()   },
+    '\u{d}' => { "\\r".to_string()   },
+    _       => { format!("NOTHING FOR {:?}", c) },
+  }
 }
