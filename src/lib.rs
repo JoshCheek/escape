666f22f1f0fad3c2da22213f505d89dee1b6b471
Josh Cheek <josh.cheek@gmail.com>
2015-05-24 17:25:52 -0600

    Zero through 6

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,12 @@ pub fn escape_string(str:&str) -> String {
 }
 
 pub fn escape_char(c:char) -> String {
-  c.to_string()
+       if '\u{0}' == c { "\\x00".to_string() }
+  else if '\u{1}' == c { "\\x01".to_string() }
+  else if '\u{2}' == c { "\\x02".to_string() }
+  else if '\u{3}' == c { "\\x03".to_string() }
+  else if '\u{4}' == c { "\\x04".to_string() }
+  else if '\u{5}' == c { "\\x05".to_string() }
+  else if '\u{6}' == c { "\\x06".to_string() }
+  else            { c.to_string() }
 }
