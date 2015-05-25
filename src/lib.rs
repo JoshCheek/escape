243b80eab2f734eb0bbcde2911eb792b404c97e5
Josh Cheek <josh.cheek@gmail.com>
2015-05-24 18:09:10 -0600

    Optimize paths

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,12 @@ pub fn escape_string(str:&str) -> String {
 
 pub fn escape_char(c:char) -> String {
   let cu32 = c as u32;
-  if      cu32 <    7 { format!("\\x{:02X}", cu32) }
+  if cu32 < 7                 ||
+     (13 < cu32 && cu32 < 27) ||
+     (27 < cu32 && cu32 < 32) ||
+     127 == cu32 { format!("\\x{:02X}", cu32) }
+  else if 32  <= cu32 && cu32 < 127 { c.to_string() }
+  else if 127 <= cu32 { c.to_string() }
   else if cu32 ==   7 { "\\a".to_string() }
   else if cu32 ==   8 { "\\b".to_string() }
   else if cu32 ==   9 { "\\t".to_string() }
@@ -14,10 +19,6 @@ pub fn escape_char(c:char) -> String {
   else if cu32 ==  11 { "\\v".to_string() }
   else if cu32 ==  12 { "\\f".to_string() }
   else if cu32 ==  13 { "\\r".to_string() }
-  else if cu32 <   27 { format!("\\x{:02X}", cu32) }
   else if cu32 ==  27 { "\\e".to_string() }
-  else if cu32 <   32 { format!("\\x{:02X}", cu32) }
-  else if cu32 <  127 { c.to_string() }
-  else if cu32 == 127 { format!("\\x{:02X}", cu32) }
-  else { c.to_string() }
+  else { "irrelevant".to_string() }
 }
