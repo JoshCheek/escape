2494dffed488fe6842d8bc2bdcd74a857fd13e66
Josh Cheek <josh.cheek@gmail.com>
2015-05-24 18:14:42 -0600

    Back to the match!

--- src/lib.rs
+++ src/lib.rs
@@ -6,19 +6,21 @@ pub fn escape_string(str:&str) -> String {
 
 pub fn escape_char(c:char) -> String {
   let cu32 = c as u32;
-  if cu32 < 7                 ||
-     (13 < cu32 && cu32 < 27) ||
-     (27 < cu32 && cu32 < 32) ||
-     127 == cu32 { format!("\\x{:02X}", cu32) }
-  else if 32  <= cu32 && cu32 < 127 { c.to_string() }
-  else if 127 <= cu32 { c.to_string() }
-  else if cu32 ==   7 { "\\a".to_string() }
-  else if cu32 ==   8 { "\\b".to_string() }
-  else if cu32 ==   9 { "\\t".to_string() }
-  else if cu32 ==  10 { "\\n".to_string() }
-  else if cu32 ==  11 { "\\v".to_string() }
-  else if cu32 ==  12 { "\\f".to_string() }
-  else if cu32 ==  13 { "\\r".to_string() }
-  else if cu32 ==  27 { "\\e".to_string() }
-  else { "irrelevant".to_string() }
+
+  match cu32 {
+    32 ... 126 => { c.to_string() },
+    0  ...  6 |
+    14 ... 26 |
+    28 ... 31 |
+    127 => { format!("\\x{:02X}", cu32) },
+    7   => { "\\a".to_string() },
+    8   => { "\\b".to_string() },
+    9   => { "\\t".to_string() },
+    10  => { "\\n".to_string() },
+    11  => { "\\v".to_string() },
+    12  => { "\\f".to_string() },
+    13  => { "\\r".to_string() },
+    27  => { "\\e".to_string() },
+    _   => { c.to_string()     },
+  }
 }
